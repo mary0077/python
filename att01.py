dd89209098de8f2b8c17c703357e22c9996e282c
mary0077 <marianagomes201@hotmail.com>
2025-05-08 22:24:32 -0300

    first commit

---
+++ att01.py
@@ -0,0 +1,10 @@
+# Este programa calcula a média, a maior e a menor idade de um grupo de pessoas.
+
+idades = [18, 21, 30, 25, 40, 29]
+media = sum(idades) / len(idades)
+maior = max(idades)
+menor = min(idades)
+
+print(f"Média de idade: {media}")
+print(f"Maior idade: {maior}")
+print(f"Menor idade: {menor}")
---
+++ att02.py
@@ -0,0 +1,4 @@
+# Exercício 2: Crie um programa que pergunte o nome do usuário e imprima uma mensagem de boas-vindas.
+
+nome = input("Qual o seu nome? ")
+print("Olá,", nome)
